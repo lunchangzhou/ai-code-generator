e690f6e69fb531157e1481423b7954d7e14ca4d6
lunchangzhou <12278949+on-chang-zhou@user.noreply.gitee.com>
2026-02-09 15:03:11 +0800

    第 6 期 - 工程项目生成（生成 Vue 工程项目） 1. 新增 Vue 工程推理流式模型配置 2. 新增 Vue 工程 AI Service 生成方法 3. 根据生成模式选择 AI Service 服务（对应修改缓存）

--- ai-code-generator-backend/src/test/java/com/lcz/aicodegenerator/AiCodeGeneratorFacadeTest.java
+++ ai-code-generator-backend/src/test/java/com/lcz/aicodegenerator/AiCodeGeneratorFacadeTest.java
@@ -34,4 +34,18 @@ class AiCodeGeneratorFacadeTest {
         Assertions.assertNotNull(completeContent);
     }
 
+    @Test
+    void generateVueProjectCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(
+                "简单的登录页面，总代码量不超过 20 行",
+                CodeGenTypeEnum.VUE_PROJECT, 1L);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
+
+
 }
