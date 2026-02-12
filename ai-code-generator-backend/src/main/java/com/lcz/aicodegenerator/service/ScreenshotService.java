f685915fa0bc0d564c1c6f4ccc5645604521cb45
lunchangzhou <12278949+on-chang-zhou@user.noreply.gitee.com>
2026-02-12 15:47:36 +0800

    第 7 期 - 功能扩展（截图服务） 1. 引入 Selenium + WebDriverManager 实现截图 2. 编写通用的截图服务，并整合进业务 3. 引入 COS 对象存储，编写 CosManager 对象存储服务

---
+++ ai-code-generator-backend/src/main/java/com/lcz/aicodegenerator/service/ScreenshotService.java
@@ -0,0 +1,10 @@
+package com.lcz.aicodegenerator.service;
+
+/**
+ * @author 1onetw
+ * @version 1.0
+ * @Description:
+ */
+public interface ScreenshotService {
+    String generateAndUploadScreenshot(String webUrl);
+}
--- ai-code-generator-backend/src/main/java/com/lcz/aicodegenerator/service/impl/AppServiceImpl.java
+++ ai-code-generator-backend/src/main/java/com/lcz/aicodegenerator/service/impl/AppServiceImpl.java
@@ -22,6 +22,7 @@ import com.lcz.aicodegenerator.model.dto.vo.AppVO;
 import com.lcz.aicodegenerator.model.dto.vo.UserVO;
 import com.lcz.aicodegenerator.service.AppService;
 import com.lcz.aicodegenerator.service.ChatHistoryService;
+import com.lcz.aicodegenerator.service.ScreenshotService;
 import com.lcz.aicodegenerator.service.UserService;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
@@ -57,6 +58,8 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
     private StreamHandlerExecutor streamHandlerExecutor;
     @Resource
     private VueProjectBuilder vueProjectBuilder;
+    @Resource
+    private ScreenshotService screenshotService;
 
     @Override
     public AppVO getAppVO(App app) {
@@ -202,9 +205,35 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         updateApp.setDeployedTime(LocalDateTime.now());
         boolean updateResult = this.updateById(updateApp);
         ThrowUtils.throwIf(!updateResult, ErrorCode.OPERATION_ERROR, "更新应用部署信息失败");
-        // 9. 返回可访问的 URL
-        return String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        // 9. 构建应用访问 URL
+        String appDeployUrl = String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        // 10. 异步生成截图并更新应用封面
+        generateAppScreenshotAsync(appId, appDeployUrl);
+        return appDeployUrl;
+
     }
+
+    /**
+     * 异步生成应用截图并更新封面
+     *
+     * @param appId  应用ID
+     * @param appUrl 应用访问URL
+     */
+    @Override
+    public void generateAppScreenshotAsync(Long appId, String appUrl) {
+        // 使用虚拟线程异步执行
+        Thread.startVirtualThread(() -> {
+            // 调用截图服务生成截图并上传
+            String screenshotUrl = screenshotService.generateAndUploadScreenshot(appUrl);
+            // 更新应用封面字段
+            App updateApp = new App();
+            updateApp.setId(appId);
+            updateApp.setCover(screenshotUrl);
+            boolean updated = this.updateById(updateApp);
+            ThrowUtils.throwIf(!updated, ErrorCode.OPERATION_ERROR, "更新应用封面字段失败");
+        });
+    }
+
 
     /**
      * 删除应用时关联删除对话历史
